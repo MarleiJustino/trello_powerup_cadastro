216357628a37e6528790f474e76f58da9533da55
MarleiJustino <marleipj@gmail.com>
2025-04-23 19:49:14 -0300

    client.js

--- client.js
+++ client.js
@@ -5,13 +5,17 @@ TrelloPowerUp.initialize({
     return [{
       icon: 'https://stalwart-strudel-8b9cf2.netlify.app/icon.png',
       text: 'Cadastrar Cliente',
-      callback: function(t) {
-        return t.popup({
-          title: 'Cadastro de Cliente',
-          url: 'index.html',
-          height: 600
-        });
-      }
+      callback: 'abrirFormulario' // nome da função callback declarada abaixo
     }];
+  },
+
+  'functions': {
+    abrirFormulario: function(t) {
+      return t.popup({
+        title: 'Cadastro de Cliente',
+        url: 'index.html',
+        height: 600
+      });
+    }
   }
 });
